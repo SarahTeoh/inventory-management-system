81e3535501ae7c6478c93b4ec0f6864d56151695
sarah <sarah.teoh752@gmail.com>
2024-05-31 09:45:57 +0800

    feat: make frontend responsive

--- frontend-react/src/components/page.tsx
+++ frontend-react/src/components/page.tsx
@@ -100,7 +100,7 @@ export default function MainPage() {
     }, []);
 
     return (
-        <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
             <div className="flex items-center justify-between space-y-2">
                 <h2 className="text-2xl font-bold tracking-tight">Inventory Management System</h2>
             </div>
